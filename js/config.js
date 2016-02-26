4219deee48a4c7c2acc7b07f028eb4c1e5754955
snakepeople <mr.astronaughty@gmail.com>
2016-02-26 09:16:35 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "LUGNUTS";
+var l_serverName = "Lugnuts Server";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
