adf91edbb8d2b1b401d4d11d2352065c6cbb314a
snakepeople <mr.astronaughty@gmail.com>
2016-02-26 10:37:47 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -95,7 +95,7 @@ var l_musicRandom = true;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 10;
+var l_musicVolume = 4;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
